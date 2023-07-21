101b1968c83e9e7d3a1a1e186c9bed4d42cd1c72
fjh-why <120797607+fjh-why@users.noreply.github.com>
2023-07-21 18:57:23 +0800

    assets 1.0

--- src/main/java/com/lc/demo/mapper/AssetsMapper.java
+++ src/main/java/com/lc/demo/mapper/AssetsMapper.java
@@ -1,4 +1,46 @@
 package com.lc.demo.mapper;
 
+import com.lc.demo.bean.Assets;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.time.LocalDateTime;
+import java.util.List;
+@Mapper
 public interface AssetsMapper {
+    //修改数据库
+
+    @Update("update assets set disposeable_assets = #{disposableAssets},totalAssets = #{totalAssets} ")
+    int updateeAssets(float disposableAssets,float totalAssets);
+
+    @Update("update assets set disposeable_assets = #{disposableAssets},totalAssets = #{totalAssets} ")
+    int updateDisposeableAssets(float disposableAssets);
+
+
+
+    @Select("select * from assets")
+    List<Assets> all();
+    @Select("select * from assets WHERE time = (SELECT MAX(time) FROM assets)")
+    Assets newAssets();
+
+    @Insert("insert into assets (total_assets,disposable_assets,time)" +
+            "VALUES" +
+            "(#{totalAssets},#{disposableAssets},#{time}")
+    int insert(float total, float disposable, LocalDateTime now);
+
+
+
+//
+
+
+
+
+
+
+
+
+
+
 }
--- src/main/java/com/lc/demo/service/Assets_LogService.java
+++ src/main/java/com/lc/demo/service/Assets_LogService.java
@@ -1,4 +1,49 @@
 package com.lc.demo.service;
 
+import com.lc.demo.bean.Assets_Log;
+
+import java.util.List;
+
 public interface Assets_LogService {
+
+    List<Assets_Log> getAllAssets_Logs();
+    
+    /**
+     * 删除
+     * @param   assetsLogId
+     * @return
+     */
+    int deleteByAssets_Log(int assetsLogId);
+    
+    
+    /**
+     * 添加
+     * @param  id
+     * @param change_assets
+     * @param  description
+     * @return
+     */
+     void addAssets_Log(int  id,float change_assets,String description ) ;
+
+    /**
+     * 修改信息
+     * @param assetsLog
+     */
+    int  update(Assets_Log assetsLog);
+    
+    
+    /**
+     * 根据id查询
+     * @param assetsLogId
+     * @return
+     */
+//    日志不可修改
+    List<Assets_Log> selectById(int assetsLogId);
+    /**
+     * 根据name查询
+     * @param userName
+     * @return
+     */
+    List<Assets_Log>  selectByName(String userName);
+
 }
