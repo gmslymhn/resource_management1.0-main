e61ca861462b28a10e59b3236e8dd24fd2d6e93c
gmslymhn <gmslymhn@gmail.com>
2023-07-21 09:35:00 +0800

    更新

--- src/main/java/com/lc/demo/service/impl/ReportServicelmpl.java
+++ src/main/java/com/lc/demo/service/impl/ReportServicelmpl.java
@@ -1,6 +1,40 @@
 package com.lc.demo.service.impl;
 
-public class ReportServicelmpl {
+import com.lc.demo.bean.Report;
+import com.lc.demo.service.ReportService;
+"C:\Users\gmsly\Documents\GitHub\resource_management1.0-main"
+import java.util.List;
+
+public class ReportServicelmpl implements ReportService {
 
 
+    @Override
+    public List<Report> getALLReport() {
+        return null;
+    }
+
+    @Override
+    public Report selectById(int goodsId) {
+        return null;
+    }
+
+    @Override
+    public int addReport(Report report) {
+        return 0;
+    }
+
+    @Override
+    public List<Report> selectByReportName(String reportName) {
+        return null;
+    }
+
+    @Override
+    public List<Report> selectByDisposeName(String dipsoseName) {
+        return null;
+    }
+
+    @Override
+    public int delecteReport(int goodsId) {
+        return 0;
+    }
 }
