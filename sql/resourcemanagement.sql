77d582f683094ad2df646d59a644350313003907
fjh-why <120797607+fjh-why@users.noreply.github.com>
2023-07-23 11:51:44 +0800

    assets sql

--- sql/resourcemanagement.sql
+++ sql/resourcemanagement.sql
@@ -22,8 +22,11 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `assets`;
 CREATE TABLE `assets`  (
-  `total_assets` float NULL DEFAULT NULL COMMENT '总资产',
-  `disposable_assets` float NULL DEFAULT NULL COMMENT '可支配资产'
+                         total_assets      float    not null comment '总资产',
+                         disposable_assets float    not null comment '可支配资产',
+                         time              datetime not null comment '最后一次修改时间',
+                         id                int auto_increment
+                           primary key
 ) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_croatian_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
@@ -35,13 +38,14 @@ CREATE TABLE `assets`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `assets_log`;
 CREATE TABLE `assets_log`  (
-  `user_name` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_croatian_ci NULL DEFAULT NULL COMMENT '管理',
-  `description` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_croatian_ci NULL DEFAULT NULL COMMENT '描述',
-  `change_assets` float NULL DEFAULT NULL COMMENT '花了多少',
-  `processing
-_time` datetime NULL DEFAULT NULL COMMENT '处理时间',
-  `before_assets` float NULL DEFAULT NULL COMMENT '花费前可支配资产',
-  `afterwards_assets` float NULL DEFAULT NULL COMMENT '花费后可支配资产'
+                             assets_log_id     int auto_increment comment '日志id'
+                               primary key,
+                             user_name         varchar(255) not null comment '管理',
+                             description       varchar(255) not null comment '描述',
+                             change_assets     float        not null comment '花了多少',
+                             process_time      datetime     not null comment '处理时间',
+                             before_assets     float        not null comment '花费前可支配资产',
+                             afterwards_assets float        not null comment '花费后可支配资产'
 ) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_croatian_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
@@ -69,12 +73,17 @@ CREATE TABLE `goods`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `report`;
 CREATE TABLE `report`  (
-  `goods_id` int NOT NULL COMMENT '损坏物品id',
-  `goods_name` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_croatian_ci NULL DEFAULT NULL COMMENT '物品名称',
-  `damage_description` text CHARACTER SET utf8mb3 COLLATE utf8mb3_croatian_ci NULL COMMENT '损坏描述',
-  `reporting_time` datetime NULL DEFAULT NULL COMMENT '上报时间',
-  `goods_state` int NULL DEFAULT NULL COMMENT '状态 已处理或未处理',
-  PRIMARY KEY (`goods_id`) USING BTREE
+                         sequence_id        int          not null comment '上报id',
+                         report_name        varchar(255) not null comment '上报人',
+                         goods_id           int          not null comment '损坏物品id',
+                         goods_name         varchar(255) not null comment '物品名称',
+                         damage_description text         not null comment '损坏描述',
+                         reporting_time     datetime     not null comment '上报时间',
+                         process_time       datetime     not null comment '处理时间',
+                         goods_state        int          not null comment '未处理1    已处理已同意2   已处理但未同意3',
+                         dispose_name       varchar(255) not null comment '处理人',
+                         primary key (goods_id, sequence_id)
+)
 ) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_croatian_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
